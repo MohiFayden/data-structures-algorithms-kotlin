efb9ab96fc6d06fd94b222dab3292cf189346b6a
Mohammad <mohammad.feyzian@gmail.com>
2024-06-18 23:32:32 -0700

    Solve 1004. Max Consecutive Ones III problem from LeetCode

---
+++ src/arrays_and_strings/LC1004MaxConsecutiveOnesIII.kt
@@ -0,0 +1,53 @@
+package arrays_and_strings
+
+import kotlin.math.max
+
+/**
+ * 1004. Max Consecutive Ones III
+ *
+ * Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+ * Output: 6
+ * Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+ * Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ * Example 2:
+ *
+ * Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+ * Output: 10
+ * Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+ * Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 105
+ * nums[i] is either 0 or 1.
+ * 0 <= k <= nums.length
+ */
+
+class LC1004MaxConsecutiveOnesIII {
+    fun longestOnes(nums: IntArray, k: Int): Int {
+        if(nums.size < k) return 0
+
+        var ans = 0
+        var curr = 0
+        var left = 0
+
+        for(right in nums.indices){
+            if(nums[right] == 0) curr++
+
+            while(curr > k){
+                if(nums[left] == 0) curr--
+                left++
+            }
+
+            ans = max(ans, right - left + 1)
+        }
+
+        return ans
+    }
+}
