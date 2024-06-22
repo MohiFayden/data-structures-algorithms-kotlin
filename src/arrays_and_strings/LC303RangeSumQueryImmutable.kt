682125271b5e64a681faa5cd48ec8709a3910983
Mohammad <mohammad.feyzian@gmail.com>
2024-06-22 01:50:31 -0700

    Solve 303. Range Sum Query - Immutable problem from LeetCode

---
+++ src/arrays_and_strings/LC303RangeSumQueryImmutable.kt
@@ -0,0 +1,62 @@
+package arrays_and_strings
+
+/**
+ * 303. Range Sum Query - Immutable
+ *
+ * Given an integer array nums, handle multiple queries of the following type:
+ *
+ * Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+ * Implement the NumArray class:
+ *
+ * NumArray(int[] nums) Initializes the object with the integer array nums.
+ * int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ *
+ *
+ * Example 1:
+ *
+ * Input
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * Output
+ * [null, 1, -1, -3]
+ *
+ * Explanation
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+ * numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+ * numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 104
+ * -105 <= nums[i] <= 105
+ * 0 <= left <= right < nums.length
+ * At most 104 calls will be made to sumRange.
+ */
+
+class LC303RangeSumQueryImmutable(val nums: IntArray) {
+
+    // To improve the performance and time complexity, we can initialize the prefix sum array
+    // At each query, we just grab the difference of the prefix sum for the left and right
+
+    private val prefix: IntArray = IntArray(nums.size) { 0 }
+
+    init {
+        prefix[0] = nums[0]
+        for (i in 1..<nums.size) {
+            prefix[i] = nums[i] + prefix[i - 1]
+        }
+    }
+
+    fun sumRange(left: Int, right: Int): Int {
+        return prefix[right] - prefix[left] + nums[left]
+    }
+
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
