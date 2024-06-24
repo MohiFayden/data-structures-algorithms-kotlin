c763489eded00eedb628d7659cf39804a9cb2fd7
Mohammad <mohammad.feyzian@gmail.com>
2024-06-24 13:07:45 -0700

    Solve 1394. Find Lucky Integer in an Array problem from LeetCode

---
+++ src/hashing/LC1394FindLuckyIntegerInAnArray.kt
@@ -0,0 +1,54 @@
+package hashing
+
+/**
+ * 1394. Find Lucky Integer in an Array
+ *
+ * Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+ *
+ * Return the largest lucky integer in the array. If there is no lucky integer return -1.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: arr = [2,2,3,4]
+ * Output: 2
+ * Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+ * Example 2:
+ *
+ * Input: arr = [1,2,2,3,3,3]
+ * Output: 3
+ * Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+ * Example 3:
+ *
+ * Input: arr = [2,2,2,3,3]
+ * Output: -1
+ * Explanation: There are no lucky numbers in the array.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= arr.length <= 500
+ * 1 <= arr[i] <= 500
+ */
+
+class LC1394FindLuckyIntegerInAnArray {
+    fun findLucky(arr: IntArray): Int {
+
+        // Define a hash table to count the frequencies
+        // Filter the items by lucky integer condition
+        // Return the max of them
+
+        val counts = mutableMapOf<Int, Int>()
+
+        // Count the frequencies
+        arr.forEach { num ->
+            counts[num] = (counts[num] ?: 0) + 1
+        }
+
+        // A simple iteration can be done to find the max, but we can simplify it by these functions:
+        val max = counts.filter { it.key == it.value }.maxByOrNull { it.key }
+
+        return max?.value ?: -1
+    }
+}
